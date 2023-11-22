0a58cd4ea516ba4b3d79f375548ef63b40dae922
ACHMEDIUS <98095760+ACHMEDIUS@users.noreply.github.com>
2023-11-22 16:34:09 +0100

    start assignment 3 v0.1

--- assignment 3/main.py
+++ assignment 3/main.py
@@ -1,168 +1,167 @@
+import re
 import sys
 import zipfile
-import zipfile
 
-# Define token types
-LPAREN = '('
-RPAREN = ')'
-LAMBDA = '\\'
-VAR = 'var'
-APP = 'app'
-EOF = 'eof'
+# # Token types
+# LPAREN = '('
+# RPAREN = ')'
+# LAMBDA = '\\'
+# VAR = 'var'
+# APP = 'app'
+# EOF = 'eof'
 
-class Token:
-    def __init__(self, type, value=None):
-        self.type = type
+# Token types
+LPAREN = 'LPAREN'
+RPAREN = 'RPAREN'
+LAMBDA = 'LAMBDA'
+ARROW = 'ARROW'
+COLON = 'COLON'
+VAR = 'VAR'
+
+# Regular expressions for tokenizing
+token_patterns = [
+    (re.compile(r'\('), LPAREN),
+    (re.compile(r'\)'), RPAREN),
+    (re.compile(r'\\|λ'), LAMBDA),
+    (re.compile(r'->'), ARROW),
+    (re.compile(r':'), COLON),
+    (re.compile(r'[a-zA-Z][a-zA-Z0-9]*'), VAR),
+]
+
+# Tokenize the input string
+def tokenize(input_str):
+    tokens = []
+    while input_str:
+        for pattern, token_type in token_patterns:
+            match = pattern.match(input_str)
+            if match:
+                tokens.append((token_type, match.group()))
+                input_str = input_str[match.end():].lstrip()
+                break
+        else:
+            raise ValueError(f"Invalid token: {input_str}")
+    return tokens
+
+# AST Node class
+class Node:
+    def __init__(self, value, children=None):
         self.value = value
+        self.children = children or []
 
-    def __repr__(self):
-        if self.value:
-            return f'Token({self.type}, {self.value})'
-        else:
-            return f'Token({self.type})'
-
-class Lexer:
-    def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.current_char = self.text[self.pos]
-
-    def error(self):
-        raise Exception('Invalid character')
-
-    def advance(self):
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None
-        else:
-            self.current_char = self.text[self.pos]
-
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-    def get_var(self):
-        result = ''
-        while self.current_char is not None and self.current_char.isalnum():
-            result += self.current_char
-            self.advance()
-        return result
-
-    def get_next_token(self):
-        while self.current_char is not None:
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-
-            if self.current_char == LPAREN:
-                self.advance()
-                return Token(LPAREN)
-
-            if self.current_char == RPAREN:
-                self.advance()
-                return Token(RPAREN)
-
-            if self.current_char == LAMBDA:
-                self.advance()
-                var = self.get_var()
-                return Token(LAMBDA, var)
-
-            if self.current_char.isalpha():
-                var = self.get_var()
-                return Token(VAR, var)
-
-            self.error()
-
-        return Token(EOF)
-
-class Parser:
-    def __init__(self, lexer):
-        self.lexer = lexer
-        self.current_token = self.lexer.get_next_token()
-
-    def error(self):
-        raise Exception('Invalid syntax')
-
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
-        else:
-            self.error()
-
-    def var(self):
-        token = self.current_token
-        self.eat(VAR)
-        return token.value
-
-    def factor(self):
-        token = self.current_token
-        if token.type == LPAREN:
-            self.eat(LPAREN)
-            expr = self.expr()
-            self.eat(RPAREN)
-            return expr
-        elif token.type == LAMBDA:
-            self.eat(LAMBDA)
-            var = self.var()
-            self.eat('.')
-            expr = self.expr()
-            return (LAMBDA, var, expr)
-        elif token.type == VAR:
-            return self.var()
-
-    def term(self):
-        left = self.factor()
-
-        while self.current_token.type == APP:
-            self.eat(APP)
-            right = self.factor()
-            left = (APP, left, right)
-
-        return left
-
-    def expr(self):
-        return self.term()
-
-class Interpreter:
-    def __init__(self):
-        pass
-
-    def eval(self, expr, env=None):
-        if env is None:
-            env = {}
-
-        if isinstance(expr, str):
-            return env.get(expr, expr)
-        elif expr[0] == LAMBDA:
-            return (LAMBDA, expr[1], self.eval(expr[2], env))
-        elif expr[0] == APP:
-            left = self.eval(expr[1], env)
-            right = self.eval(expr[2], env)
-            if left[0] == LAMBDA:
-                return self.eval(left[2], {**env, left[1]: right})
+# Parse the tokens into an abstract syntax tree (AST)
+def parse(tokens):
+    def parse_expr():
+        token_type, token_value = tokens.pop(0)
+        if token_type == LAMBDA:
+            var_token = tokens.pop(0)
+            if var_token[0] == VAR:
+                type_token = tokens.pop(0)
+                if type_token[0] == ARROW:
+                    return Node('lambda', [var_token[1], type_token[1], parse_expr()])
+                else:
+                    raise ValueError("Missing '->' after lambda expression type.")
             else:
-                return (APP, left, right)
+                raise ValueError("Invalid variable after lambda.")
+        elif token_type == VAR:
+            return Node('var', [token_value])
+        elif token_type == LPAREN:
+            expr = parse_expr()
+            if tokens[0][0] == RPAREN:
+                tokens.pop(0)  # Consume the closing parenthesis
+                return expr
+            else:
+                raise ValueError("Mismatched parentheses.")
+        else:
+            raise ValueError(f"Unexpected token: {token_type}")
 
+    def parse_type():
+        token_type, token_value = tokens.pop(0)
+        if token_type == VAR:
+            return Node('type_var', [token_value])
+        elif token_type == LPAREN:
+            type_expr = parse_type()
+            if tokens[0][0] == RPAREN:
+                tokens.pop(0)  # Consume the closing parenthesis
+                return type_expr
+            else:
+                raise ValueError("Mismatched parentheses in type.")
+        else:
+            raise ValueError(f"Unexpected token in type: {token_type}")
+
+    expr = parse_expr()
+    if tokens[0][0] == COLON:
+        tokens.pop(0)  # Consume the colon
+        type_expr = parse_type()
+        return Node('judgement', [expr, type_expr])
+    else:
+        raise ValueError("Missing ':' in judgement.")
+
+# Perform type checking on the AST
+def type_check(ast):
+    def check_judgement(node, context):
+        expr, type_expr = node.children
+        if check_expr(expr, context):
+            return check_type(type_expr, context)
+        else:
+            raise ValueError("Expression does not type-check.")
+
+    def check_expr(node, context):
+        if node.value == 'var':
+            var_name = node.children[0]
+            if var_name in context:
+                return True
+            else:
+                raise ValueError(f"Variable {var_name} has an unknown type.")
+        elif node.value == 'lambda':
+            var_name, type_var, body = node.children
+            new_context = {var_name: type_var}
+            new_context.update(context)
+            return check_expr(body, new_context)
+        else:
+            raise ValueError("Invalid expression node.")
+
+    def check_type(node, context):
+        if node.value == 'type_var':
+            type_var = node.children[0]
+            if type_var in context:
+                return True
+            else:
+                raise ValueError(f"Type variable {type_var} is not declared.")
+        elif node.value == 'arrow':
+            left_type, right_type = node.children
+            return check_type(left_type, context) and check_type(right_type, context)
+        else:
+            raise ValueError("Invalid type node.")
+
+    return check_judgement(ast, {})
 
 def read_zip_file(file_path):
     with zipfile.ZipFile(file_path, 'r') as zip_file:
         for file_name in zip_file.namelist():
             with zip_file.open(file_name) as file:
-                # Do something with the file contents
-                print(file.read())
+                # Read the file contents
+                content = file.read().decode('utf-8')
 
+                # Print the result
 
+# Main function to read input, tokenize, parse, and type-check
 def main():
-    # text = input("Enter the lambda calculus expression: ")
-    # lexer = Lexer(text)
-    # parser = Parser(lexer)
-    # interpreter = Interpreter()
-    # expr = parser.expr()
-    # result = interpreter.eval(expr)
-    file_name = input("Enter the name of the zip file: ")
-    # read_zip_file(file_name)
-    # print(result)
-    print(read_zip_file(file_name))
+    if len(sys.argv) != 2:
+        print("Usage: python program.py <input_file>")
+        sys.exit(1)
 
-if __name__ == '__main__':
+    input_file = sys.argv[1]
+    with open(input_file, 'r') as file:
+        input_str = file.read()
+
+    try:
+        tokens = tokenize(input_str)
+        ast = parse(tokens)
+        type_check(ast)
+        print("Judgement is derivable.")
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
     main()
