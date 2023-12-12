12df1397480a9300b82a1cfbcc295a43f6005c49
ACHMEDIUS <98095760+ACHMEDIUS@users.noreply.github.com>
2023-12-12 23:55:47 +0100

    Assignment 3

--- assignment 3/main.py
+++ assignment 3/main.py
@@ -1,167 +1,177 @@
-import re
-import sys
 import zipfile
-
-# # Token types
-# LPAREN = '('
-# RPAREN = ')'
-# LAMBDA = '\\'
-# VAR = 'var'
-# APP = 'app'
-# EOF = 'eof'
+import tarfile
+import os
 
 # Token types
+VAR = 'VAR'
 LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
 LAMBDA = 'LAMBDA'
+SEPARATOR = 'SEPARATOR'
 ARROW = 'ARROW'
 COLON = 'COLON'
-VAR = 'VAR'
 
-# Regular expressions for tokenizing
-token_patterns = [
-    (re.compile(r'\('), LPAREN),
-    (re.compile(r'\)'), RPAREN),
-    (re.compile(r'\\|λ'), LAMBDA),
-    (re.compile(r'->'), ARROW),
-    (re.compile(r':'), COLON),
-    (re.compile(r'[a-zA-Z][a-zA-Z0-9]*'), VAR),
-]
-
-# Tokenize the input string
-def tokenize(input_str):
+def lexer(input_string):
     tokens = []
-    while input_str:
-        for pattern, token_type in token_patterns:
-            match = pattern.match(input_str)
-            if match:
-                tokens.append((token_type, match.group()))
-                input_str = input_str[match.end():].lstrip()
-                break
+    current_token = ''
+
+    for char in input_string:
+        if char.isalnum() or char in ['λ', '\\']:
+            current_token += char
         else:
-            raise ValueError(f"Invalid token: {input_str}")
+            if current_token:
+                tokens.append((VAR, current_token))
+                current_token = ''
+
+            if char == '(':
+                tokens.append((LPAREN, char))
+            elif char == ')':
+                tokens.append((RPAREN, char))
+            elif char == 'λ' or char == '\\':
+                tokens.append((LAMBDA, char))
+            elif char == ';':
+                tokens.append((SEPARATOR, char))
+            elif char == '-':
+                tokens.append((ARROW, char))
+            elif char == ':':
+                tokens.append((COLON, char))
+
+    # Check for the last token if any
+    if current_token:
+        tokens.append((VAR, current_token))
+
     return tokens
 
-# AST Node class
-class Node:
-    def __init__(self, value, children=None):
-        self.value = value
-        self.children = children or []
+def read_archive(file_path):
+    contents = ""  # Initialize an empty string to store the contents
+    # Determine the file type based on the extension
+    _, file_extension = os.path.splitext(file_path)
 
-# Parse the tokens into an abstract syntax tree (AST)
-def parse(tokens):
-    def parse_expr():
-        token_type, token_value = tokens.pop(0)
-        if token_type == LAMBDA:
-            var_token = tokens.pop(0)
-            if var_token[0] == VAR:
-                type_token = tokens.pop(0)
-                if type_token[0] == ARROW:
-                    return Node('lambda', [var_token[1], type_token[1], parse_expr()])
-                else:
-                    raise ValueError("Missing '->' after lambda expression type.")
-            else:
-                raise ValueError("Invalid variable after lambda.")
-        elif token_type == VAR:
-            return Node('var', [token_value])
-        elif token_type == LPAREN:
-            expr = parse_expr()
-            if tokens[0][0] == RPAREN:
-                tokens.pop(0)  # Consume the closing parenthesis
-                return expr
-            else:
-                raise ValueError("Mismatched parentheses.")
-        else:
-            raise ValueError(f"Unexpected token: {token_type}")
-
-    def parse_type():
-        token_type, token_value = tokens.pop(0)
-        if token_type == VAR:
-            return Node('type_var', [token_value])
-        elif token_type == LPAREN:
-            type_expr = parse_type()
-            if tokens[0][0] == RPAREN:
-                tokens.pop(0)  # Consume the closing parenthesis
-                return type_expr
-            else:
-                raise ValueError("Mismatched parentheses in type.")
-        else:
-            raise ValueError(f"Unexpected token in type: {token_type}")
-
-    expr = parse_expr()
-    if tokens[0][0] == COLON:
-        tokens.pop(0)  # Consume the colon
-        type_expr = parse_type()
-        return Node('judgement', [expr, type_expr])
-    else:
-        raise ValueError("Missing ':' in judgement.")
-
-# Perform type checking on the AST
-def type_check(ast):
-    def check_judgement(node, context):
-        expr, type_expr = node.children
-        if check_expr(expr, context):
-            return check_type(type_expr, context)
-        else:
-            raise ValueError("Expression does not type-check.")
-
-    def check_expr(node, context):
-        if node.value == 'var':
-            var_name = node.children[0]
-            if var_name in context:
-                return True
-            else:
-                raise ValueError(f"Variable {var_name} has an unknown type.")
-        elif node.value == 'lambda':
-            var_name, type_var, body = node.children
-            new_context = {var_name: type_var}
-            new_context.update(context)
-            return check_expr(body, new_context)
-        else:
-            raise ValueError("Invalid expression node.")
-
-    def check_type(node, context):
-        if node.value == 'type_var':
-            type_var = node.children[0]
-            if type_var in context:
-                return True
-            else:
-                raise ValueError(f"Type variable {type_var} is not declared.")
-        elif node.value == 'arrow':
-            left_type, right_type = node.children
-            return check_type(left_type, context) and check_type(right_type, context)
-        else:
-            raise ValueError("Invalid type node.")
-
-    return check_judgement(ast, {})
-
-def read_zip_file(file_path):
-    with zipfile.ZipFile(file_path, 'r') as zip_file:
-        for file_name in zip_file.namelist():
-            with zip_file.open(file_name) as file:
-                # Read the file contents
+    if file_extension == '.zip':
+        with zipfile.ZipFile(file_path, 'r') as zip_file:
+            for file_name in zip_file.namelist():
+                with zip_file.open(file_name) as file:
+                    # Read the file contents and append to the string
+                    content = file.read().decode('utf-8')
+                    contents += content
+    elif file_extension == '.tar.gz':
+        with tarfile.open(file_path, 'r:gz') as tar_file:
+            for tar_info in tar_file.getmembers():
+                file = tar_file.extractfile(tar_info)
+                # Read the file contents and append to the string
                 content = file.read().decode('utf-8')
+                contents += content
+    else:
+        raise ValueError(f"Unsupported archive format: {file_extension}")
+    return contents
 
-                # Print the result
+def parser(tokens):
+    expr = parse_judgement(tokens)
+    return expr
 
-# Main function to read input, tokenize, parse, and type-check
+def parse_judgement(tokens):
+    expr = parse_expr(tokens)
+    if tokens[0][0] == COLON:
+        tokens.pop(0)  # Consume ':'
+        type_expr = parse_type(tokens)
+        return (expr, type_expr)
+    else:
+        raise SyntaxError("Expected ':' in judgement")
+
+def parse_expr(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError("Unexpected end of input")
+
+    if tokens[0][0] == VAR:
+        return tokens.pop(0)
+
+    if tokens[0][0] == LPAREN:
+        tokens.pop(0)
+        expr1 = parse_expr(tokens)
+        if tokens[0][0] == SEPARATOR:
+            tokens.pop(0)
+            expr2 = parse_expr(tokens)
+            if tokens[0][0] == RPAREN:
+                tokens.pop(0)
+                return (expr1, expr2)
+            else:
+                raise SyntaxError("Expected ')'")
+
+    if tokens[0][0] == LAMBDA:
+        tokens.pop(0)
+        var = parse_expr(tokens)
+        if tokens[0][0] == ARROW:
+            tokens.pop(0)
+            type_expr = parse_type(tokens)
+            body = parse_expr(tokens)
+            return (LAMBDA, var, type_expr, body)
+        else:
+            raise SyntaxError("Expected '->' after lambda abstraction")
+
+def parse_type(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError("Unexpected end of input")
+
+    if tokens[0][0] == VAR:
+        return tokens.pop(0)
+
+    if tokens[0][0] == LPAREN:
+        tokens.pop(0)
+        type1 = parse_type(tokens)
+        if tokens[0][0] == ARROW:
+            tokens.pop(0)
+            type2 = parse_type(tokens)
+            if tokens[0][0] == RPAREN:
+                tokens.pop(0)
+                return (type1, ARROW, type2)
+            else:
+                raise SyntaxError("Expected ')' after type")
+        else:
+            raise SyntaxError("Expected '->' in type")
+    
+def beta_reduction(expr, var, arg):
+    if type(expr) == tuple and expr[0] == VAR:
+        return arg if expr[1] == var else expr
+    elif type(expr) == tuple and expr[0] == LAMBDA:
+        return (LAMBDA, expr[1], expr[2], beta_reduction(expr[3], var, arg))
+    elif type(expr) == tuple and expr[0] == LPAREN:
+        return (LPAREN, beta_reduction(expr[1], var, arg), beta_reduction(expr[2], var, arg), beta_reduction(expr[3], var, arg), RPAREN)
+    elif type(expr) == tuple and expr[0] == SEPARATOR:
+        return (SEPARATOR, beta_reduction(expr[1], var, arg), beta_reduction(expr[2], var, arg))
+    else:
+        raise TypeError(f"Invalid expression type: {type(expr)}")
+
+def to_standard_format(expr):
+    if type(expr) == tuple and expr[0] == VAR:
+        return expr[1]
+    elif type(expr) == tuple and expr[0] == LAMBDA:
+        return f"(λ{expr[1]}^{expr[2]}.{to_standard_format(expr[3])})"
+    elif type(expr) == tuple and expr[0] == LPAREN:
+        return f"({to_standard_format(expr[1])} {to_standard_format(expr[2])})"
+    elif type(expr) == tuple and expr[0] == SEPARATOR:
+        return f"{to_standard_format(expr[1])}; {to_standard_format(expr[2])}"
+    else:
+        raise TypeError(f"Invalid expression type: {type(expr)}")
+
+def output(judgement):
+    standard_format_expr = to_standard_format(judgement[0])
+    print(f"{standard_format_expr} : {to_standard_format(judgement[1])}")
+
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python program.py <input_file>")
-        sys.exit(1)
-
-    input_file = sys.argv[1]
-    with open(input_file, 'r') as file:
-        input_str = file.read()
-
+    file_name = input("Enter the name of the archive file (zip or tar.gz): ")
     try:
-        tokens = tokenize(input_str)
-        ast = parse(tokens)
-        type_check(ast)
-        print("Judgement is derivable.")
-    except ValueError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+        contents = read_archive(file_name)
 
-if __name__ == "__main__":
+        # Split the contents into lines and process each expression
+        for line in contents.splitlines():
+            tokens = lexer(line)
+            judgement = parser(tokens)
+            output(judgement)
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Exiting...")
+        return 1
+    return 0
+
+if __name__ == '__main__':
     main()
