53b7fbf941485cebb86c36598d9e97e3b34b21be
ACHMEDIUS <98095760+ACHMEDIUS@users.noreply.github.com>
2023-12-15 11:25:45 +0000

    assignment 1 read expressions from terminal

--- assignment 1/main.py
+++ assignment 1/main.py
@@ -1,5 +1,3 @@
-import zipfile
-
 # Token types
 VAR = 'VAR'
 LPAREN = 'LPAREN'
@@ -34,16 +32,6 @@ def lexer(input_string):
 
     return tokens
 
-def read_zip_file(file_path):
-    contents = ""  # Initialize an empty string to store the contents
-    with zipfile.ZipFile(file_path, 'r') as zip_file:
-        for file_name in zip_file.namelist():
-            with zip_file.open(file_name) as file:
-                # Read the file contents and append to the string
-                content = file.read().decode('utf-8')
-                contents += content
-    return contents
-
 def parser(tokens):
     expr = parse_expr(tokens)
     return expr
@@ -52,65 +40,49 @@ def parse_expr(tokens):
     if len(tokens) == 0:
         raise SyntaxError("Unexpected end of input")
 
-    if type(tokens[0]) == VAR:
+    if type(tokens[0][1]) == VAR:
         return tokens.pop(0)
 
-    if tokens[0] == LPAREN:
+    if tokens[0][1] == LPAREN:
         tokens.pop(0)
         expr = parse_expr(tokens)
-        if tokens[0] == RPAREN:
+        if tokens[0][1] == RPAREN:
             tokens.pop(0)
             return expr
         else:
             raise SyntaxError("Expected ')'")
 
-    if tokens[0] == LAMBDA:
+    if tokens[0][1] == LAMBDA:
         name = "lambda"
         tokens.pop(0)
         var = parse_expr(tokens)
-        if tokens[0] != LPAREN:
+        if tokens[0][1] != LPAREN:
             raise SyntaxError("Expected '(' after lambda abstraction")
         tokens.pop(0)
         expr = parse_expr(tokens)
-        if tokens[0] != RPAREN:
-            raise SyntaxError("Expected ')' after lambda expression")
-        tokens.pop(0)
-        return lambda var: expr
-
-    if tokens[0] == LAMBDA:
-        tokens.pop(0)
-        var = parse_expr(tokens)
-        if tokens[0] != LPAREN:
-            raise SyntaxError("Expected '(' after lambda abstraction")
-        tokens.pop(0)
-        expr = parse_expr(tokens)
-        if tokens[0] != RPAREN:
+        if tokens[0][1] != RPAREN:
             raise SyntaxError("Expected ')' after lambda expression")
         tokens.pop(0)
         return lambda var: expr
 
 def to_standard_format(expr):
-    if type(expr) == str:
-        return expr
-    elif type(expr) == type(lambda x: x):
-        var = expr.__var__
-        expr = expr(var)
+    if type(expr[1]) == str:
+        return expr[1]
+    elif type(expr[1]) == type(lambda x: x):
+        var = expr[1].__var__
+        expr = expr[1](var)
         return f"(λ{var}. {expr})"
     else:
-        raise TypeError(f"Invalid expression type: {type(expr)}")
+        raise TypeError(f"Invalid expression type: {type(expr[1])}")
 
 def output(expr):
     standard_format_expr = to_standard_format(expr)
     print(standard_format_expr)
 
 def main():
-    file_name = input("Enter the name of the zip file: ")
+    input_string = input("Enter the lambda calculus expression: ")
     try:
-        contents = read_zip_file(file_name)
-        
-        # test
-        # print(contents) 
-        tokens = lexer(contents)
+        tokens = lexer(input_string)
         expr = parser(tokens)
         output(expr)
     except Exception as e:
